c6b15736445ea1766502193282d5ef615e0769e8
nekko-master <kanomaster00@gmail.com>
2025-10-17 18:17:38 +0900

    確認依頼後の修正（2回目）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    @prototype = Prototype.find(params[:id])  # 1回目の修正：コメント投稿機能
-    @comment = Comment.new(comment_params)
+    @prototype = Prototype.find(params[:prototype_id])  # 2回目の修正：コメント投稿機能
+    @comment = @prototype.comments.new(comment_params)  # 2回目の修正：コメント投稿機能
     @comments = @prototype.comments.includes(:user)   # 1回目の修正：コメント投稿機能
 
     if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+      redirect_to prototype_path(@prototype)  #2回目の修正：コメント投稿機能
     else
       render "prototypes/show" ,status: :unprocessable_entity   # 1回目の修正：コメント投稿機能
     end
@@ -13,6 +13,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id] )
+    params.require(:comment).permit(:content).merge(user_id: current_user.id )  # 2回目の修正：コメント投稿機能
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
 
-  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
+  # resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] 2回目の修正：コメント投稿機能
   resources :prototypes do
     resources :comments, only: [:create]
   end
